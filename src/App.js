4b6cb5c4eef4ccbdafe4d540c6419b7eb8754313
reem2213 <141642956+reem2213@users.noreply.github.com>
2023-09-11 16:14:39 +0300

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import EditPost from "./components/EditPost/EditPost";
+import MenuBar from "./components/MenuBar/MenuBar";
+
+function App() {
+  return (
+    <div>
+     <MenuBar/> 
+     <EditPost/>
+    </div>
+  );
+}
+
+export default App;
